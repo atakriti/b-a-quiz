f6db8578e286588dfdd54f57b72a9028eaea594f
atakriti <anwart256@gmail.com>
2023-01-01 11:56:43 +0100

    hamburger menu done

--- client/src/Header/Header.jsx
+++ client/src/Header/Header.jsx
@@ -6,11 +6,18 @@ import { context } from '../Context'
 import jsPDF from 'jspdf';
 import logo1 from "../images/logo.png"
 import axios from 'axios'
+import { useMediaQuery } from 'react-haiku';
+import {HiMenuAlt3} from "react-icons/hi"
 function Header() {
+      const breakpoint = useMediaQuery('(max-width: 800px)');
+      let [isOpen, setIsOpen] = useState(false)
+      let [animateMenu,setAnimateMenu] = useState(false)
   let { isSignedIn,setIsSignedIn,setSignedIn,setSignedInValue,signedIn,users,fetchingUsers,setUsers,setIsDownload } = useContext(context)
   let findUser = users.find(user => user.email === signedIn.email)
   let [findUserState,setFindUserState] = useState(findUser)
       let handleSignout = () => {
+            setAnimateMenu(!animateMenu)
+            setTimeout(()=>(setIsOpen(!isOpen)),300)
             setIsSignedIn(false)
        fetchingUsers().then(result => setUsers(result)) 
 
@@ -24,6 +31,8 @@ function Header() {
             })
       }
       let handleStart = async () => {
+            setAnimateMenu(!animateMenu)
+            setTimeout(()=>(setIsOpen(!isOpen)),300)
             setFindUserState({...findUserState,a1:false,a2:false,b1:false})
             await axios.put(`http://localhost:4000/update/${findUserState._id}`, { ...findUserState, a1: false, a2: false, b1: false })
        fetchingUsers().then(result => setUsers(result)) 
@@ -46,11 +55,18 @@ function Header() {
       pdf.addImage(logo1, "png", 73, 0, 70, 70) //! Not touche it
       pdf.text(text,30,80)
       let handleDownload = () => {
+            setAnimateMenu(!animateMenu)
+            setTimeout(()=>(setIsOpen(!isOpen)),300)
           pdf.save('certificate.pdf');
-          setIsDownload(false)
+            setIsDownload(false)
+           
       }
       // ======================================== End Download ========================
-
+      let handleIsOpen = () => {
+            setAnimateMenu(!animateMenu)
+            setTimeout(()=>(setIsOpen(!isOpen)),300)
+            
+      }
 
 
       useEffect(() => {
@@ -60,29 +76,68 @@ function Header() {
       <header>
           <div className="logo">
               <Link to="/" ><img src={logo} alt="" /></Link>
-          </div>
-          <nav>
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-                    <Link to="/contact">Contact</Link>
-                    {findUserState?.b1 && (
-                          <a onClick={handleDownload}>Download Certificate</a>
+              </div>
+              
+
+              {breakpoint && (
+                    <div className="hamburgerMenu">
+                          <div onClick={handleIsOpen} className="hamburger">
+                          <HiMenuAlt3 style={isOpen && {fill:"white"}}/>
+                          </div>
+                          {isOpen && (
+                                  <div style={animateMenu ? {animation:"menuOpen 0.5s forwards"}:{animation:"menuClose 0.5s forwards"}} className="menu_">
+                                  <Link onClick={handleIsOpen}  to="/">Home</Link>
+                      <Link onClick={handleIsOpen}  to="/about">About</Link>
+                            <Link onClick={handleIsOpen}  to="/contact">Contact</Link>
+                            {findUserState?.b1 && (
+                                  <a onClick={handleDownload}>Download Certificate</a>
+                    )}
+        
+        
+                            {isSignedIn && (
+                                  <Link onClick={handleStart} to="/">Start again</Link>
+                      )}
+                {isSignedIn ? (
+                      <Link onClick={handleSignout} to="/">Sign out</Link>
+        
+                ): (
+                    
+                      <Link onClick={handleIsOpen}  to="/register">Sign in</Link>
+                            )}
+                                  </div>
+                        )}
+
+
+                    </div>
             )}
 
+{/* ========================== To hide over 800px ================ */}
 
-                    {isSignedIn && (
-                          <Link onClick={handleStart} to="/">Start again</Link>
-              )}
-        {isSignedIn ? (
-              <Link onClick={handleSignout} to="/">Sign out</Link>
-
-        ): (
-            
-              <Link to="/register">Sign in</Link>
-                    )}
-                   
+              {!breakpoint && (
                     
-          </nav>
+          <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                {findUserState?.b1 && (
+                      <a onClick={handleDownload}>Download Certificate</a>
+        )}
+
+
+                {isSignedIn && (
+                      <Link onClick={handleStart} to="/">Start again</Link>
+          )}
+    {isSignedIn ? (
+          <Link onClick={handleSignout} to="/">Sign out</Link>
+
+    ): (
+        
+          <Link to="/register">Sign in</Link>
+                )}
+               
+                
+      </nav>
+)}
     </header>
   )
 }
